eb22532e0f0cbd71ca01974b29150d079989992c
Joe Gallo <gallo.j@gmail.com>
2015-06-16 08:48:34 -0400

    * Add script to generate opml from blog urls in readme. * Add generated opml file.

---
+++ generate_opml.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require 'builder'
+require 'json'
+require 'net/http'
+require 'uri'
+
+# grab name/url pairings from README
+readme = File.open("README.md", "r")
+contents = readme.read
+matches = contents.scan(/\* (.*) (http.*)/)
+
+Struct.new("Blog", :name, :webURL, :rssURL)
+blogs = Array.new
+
+# for each blog URL, check if rss URL exists
+matches.each { |match|
+  rssCheckURL = "http://ajax.googleapis.com/ajax/services/feed/lookup?v=1.0&q=#{match[1]}"
+  uri = URI.parse(rssCheckURL)
+  response = JSON.parse(Net::HTTP.get(uri))
+  if response["responseData"] && response["responseData"].has_key?("url")
+    rssURL = response["responseData"]["url"]
+    blogs.push(Struct::Blog.new(match[0], match[1], rssURL))
+  end
+}
+
+puts blogs
+
+# write opml
+xml = Builder::XmlMarkup.new( :indent => 2 )
+xml.instruct! :xml, version: "1.0", encoding: "UTF-8"
+xml.tag!("opml", {version: "1.0"}) do
+  # head
+  xml.tag!("head") do
+    xml.title "Engineering Blogs"
+  end
+
+  # body
+  xml.tag!("body") do
+    blogs.each { |blog|
+      xml.tag!("outline", {type: "rss", text: blog.name, title: blog.name, xmlUrl: blog.rssURL, htmlUrl: blog.webURL})
+    }
+  end
+end
+
+output = File.new("engineering_blogs.opml", "wb")
+output.write(xml)
+output.close
