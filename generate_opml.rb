2267f14dc9c942fef03814c95a34e836967dfc3d
HuyNQ <huy@huynq.net>
2015-06-22 12:04:29 +0700

    Update opml generator - Add .gitignore - Wrap blog list outline in parent outline - Update opml generator - Remove Gemfile.lock

--- generate_opml.rb
+++ generate_opml.rb
@@ -5,71 +5,89 @@ require 'json'
 require 'net/http'
 require 'nokogiri'
 require 'uri'
+require 'feedbag'
 
-OUTPUT_FILENAME = "engineering_blogs.opml"
+OUTPUT_FILENAME = 'engineering_blogs.opml'
 LIMIT = 10
+TITLE = 'Engineering Blogs'
 
-# grab name/url pairings from README
-readme = File.open("README.md", "r")
+# grab name/url pairings from README.md
+readme = File.open('README.md', 'r')
 contents = readme.read
 matches = contents.scan(/\* (.*) (http.*)/)
 
-Struct.new("Blog", :name, :webURL, :rssURL)
-blogs = Array.new
+Struct.new('Blog', :name, :web_url, :rss_url)
+blogs = []
 
 # for each blog URL, check if rss URL exists
-matches.each_with_index { |match, index|
+matches.each_with_index do |match, _|
   # for testing purposes
   # if index > LIMIT
   #   break
   # end
 
   name = match[0]
-  webURL = match[1]
+  web_url = match[1]
 
-  # if rssURL already in existing opml file, use that; otherwise, do a lookup
+  # if rss_url already in existing opml file, use that; otherwise, do a lookup
+  rss_url = nil
   if File.exist?(OUTPUT_FILENAME)
     xml = Nokogiri::XML(File.open(OUTPUT_FILENAME))
-    existingBlog = xml.xpath("//outline[@htmlUrl='#{webURL}']").first
-    if existingBlog != nil
-      rssURL = existingBlog.attr('xmlUrl')
+    existing_blog = xml.xpath("//outline[@htmlUrl='#{web_url}']").first
+    unless existing_blog.nil?
+      rss_url = existing_blog.attr('xmlUrl')
       puts "#{name}: ALREADY HAVE"
     end
   end
 
-  if rssURL.nil?
+  if rss_url.nil?
     puts "#{name}: GETTING"
-    rssCheckURL = "http://ajax.googleapis.com/ajax/services/feed/lookup?v=1.0&q=#{match[1]}"
-    uri = URI.parse(rssCheckURL)
+    rss_check_url = "http://ajax.googleapis.com/ajax/services/feed/lookup?v=1.0&q=#{web_url}"
+    uri = URI.parse(rss_check_url)
     response = JSON.parse(Net::HTTP.get(uri))
-    if response["responseData"] && response["responseData"].has_key?("url")
-      rssURL = response["responseData"]["url"]
+    rss_url = response['responseData']['url'] if response['responseData'] && response['responseData'].has_key?('url')
+    unless rss_url
+      rss_url = Feedbag.find web_url
+      if rss_url.length == 0
+        %w(rss feed feeds).each do |e|
+          web_url << '/' if web_url[-1] != '/'
+          if Feedbag.feed? (web_url + e)
+            rss_url = web_url + e
+            break
+          end
+        end
+      else
+        rss_url = rss_url.first if rss_url.first.length > 0
+      end
     end
   end
 
-  if rssURL != nil
-    blogs.push(Struct::Blog.new(name, webURL, rssURL))
-  end
-}
+  blogs.push(Struct::Blog.new(name, web_url, rss_url)) if !rss_url.nil? && rss_url.length > 0
+end
+
+blogs.sort_by { |b| b.name.capitalize }
 
 # write opml
-xml = Builder::XmlMarkup.new( :indent => 2 )
-xml.instruct! :xml, version: "1.0", encoding: "UTF-8"
-xml.tag!("opml", {version: "1.0"}) do
+xml = Builder::XmlMarkup.new(indent: 2)
+xml.instruct! :xml, version: '1.0', encoding: 'UTF-8'
+xml.tag!('opml', version: '1.0') do
   # head
-  xml.tag!("head") do
-    xml.title "Engineering Blogs"
+  xml.tag!('head') do
+    xml.title TITLE
   end
 
   # body
-  xml.tag!("body") do
-    blogs.each { |blog|
-      xml.tag!("outline", {type: "rss", text: blog.name, title: blog.name, xmlUrl: blog.rssURL, htmlUrl: blog.webURL})
-    }
+  xml.tag!('body') do
+    xml.tag!('outline', text: TITLE, title: TITLE) do
+      blogs.each do |blog|
+        xml.tag!('outline', type: 'rss', text: blog.name, title: blog.name,
+          xmlUrl: blog.rss_url, htmlUrl: blog.web_url)
+      end
+    end
   end
 end
 
-output = File.new(OUTPUT_FILENAME, "wb")
+output = File.new(OUTPUT_FILENAME, 'wb')
 output.write(xml.target!)
 output.close
 
