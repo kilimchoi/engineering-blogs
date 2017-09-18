d2daeb758bc2a62082600d137090d83e2c225a1a
Albert Ho <ah323@cornell.edu>
2017-09-17 17:05:08 -0700

    Clean up some Ruby

--- generate_opml.rb
+++ generate_opml.rb
@@ -4,12 +4,12 @@ require 'builder'
 require 'feedbag'
 require 'nokogiri'
 
-OUTPUT_FILENAME = 'engineering_blogs.opml'
-TITLE = 'Engineering Blogs'
+INPUT_FILENAME = 'README.md'.freeze
+OUTPUT_FILENAME = 'engineering_blogs.opml'.freeze
+TITLE = 'Engineering Blogs'.freeze
 
 # grab name/url pairings from README.md
-readme = File.open('README.md', 'r')
-contents = readme.read
+contents = File.read INPUT_FILENAME
 matches = contents.scan(/\* (.*) (http.*)/)
 # All blogs that do not respond
 unavailable = []
@@ -35,13 +35,13 @@ Struct.new('Blog', :name, :web_url, :rss_url)
 blogs = []
 
 # for each blog URL, check if rss URL exists
-matches.each_with_index do |match, index|
+matches.each do |match|
   name = match[0]
   web_url = match[1]
 
   if temp_ignores.include?(name)
-      puts "#{name}: IGNORE [TEMPORARILY]"
-      next
+    puts "#{name}: IGNORE [TEMPORARILY]"
+    next
   end
 
   # if rss_url already in existing opml file, use that; otherwise, do a lookup
@@ -55,19 +55,16 @@ matches.each_with_index do |match, index|
   puts "#{name}: GETTING" if rss_url.nil?
   rss_url = Feedbag.find(web_url).first if rss_url.nil?
   if rss_url.nil?
-    suggested_paths = ['/rss', '/feed', '/feeds', '/atom.xml', '/feed.xml', '/rss.xml', '.atom']
+    suggested_paths = ['/rss', '/feed', '/feeds', '/atom.xml',
+                       '/feed.xml', '/rss.xml', '.atom']
     suggested_paths.each do |suggested_path|
       rss_url = Feedbag.find("#{web_url.chomp('/')}#{suggested_path}").first
       break if rss_url
     end
   end
 
-  if rss_url && rss_url.length > 0
-    blogs.push(Struct::Blog.new(name, web_url, rss_url))
-  else
-    unavailable.push(Struct::Blog.new(name, web_url, rss_url))
-  end
-
+  list = rss_url && !rss_url.empty? ? blogs : unavailable
+  list.push(Struct::Blog.new(name, web_url, rss_url))
 end
 
 blogs.sort_by { |b| b.name.capitalize }
@@ -87,7 +84,7 @@ xml.tag!('opml', version: '1.0') do
     xml.tag!('outline', text: TITLE, title: TITLE) do
       blogs.each do |blog|
         xml.tag!('outline', type: 'rss', text: blog.name, title: blog.name,
-          xmlUrl: blog.rss_url, htmlUrl: blog.web_url)
+                            xmlUrl: blog.rss_url, htmlUrl: blog.web_url)
       end
     end
   end
@@ -100,8 +97,8 @@ output.close
 puts "DONE: #{blogs.count} written to #{OUTPUT_FILENAME}"
 
 puts "\nUnable to find an RSS feed for the following blogs:"
-puts "==================================================="
+puts '==================================================='
 unavailable.each do |b|
   puts "#{b.name} | #{b.web_url}"
 end
-puts "==================================================="
+puts '==================================================='
